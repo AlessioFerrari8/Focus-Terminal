2f85da573dd25fe4a46cb3a013c3185d18b2d86e
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-04-11 15:59:36 +0200

    Feat: implement command handling and scrolling in terminal

--- focus-terminal/src/app/app.ts
+++ focus-terminal/src/app/app.ts
@@ -1,12 +1,48 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Component, computed, signal, WritableSignal,
+  ElementRef, ViewChild, AfterViewChecked
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('focus-terminal');
+
+  // per gestire scroll
+  @ViewChild('terminal') terminalRef!: ElementRef;
+  @ViewChild('cmdInput') cmdInput!: ElementRef;
+
+  // attributes
+  protected title = computed(() => 'focus-terminal');
+  protected lines: WritableSignal<string[]> = signal<string[]>([]);
+  protected command: WritableSignal<string> = signal('');
+
+  // TODO: gestire il problema che devo cliccare ogni volta
+  handleCommand() {
+    const trimmed = this.command().trim();
+    if (trimmed) {
+      this.lines.update(l => [...l, `> ${trimmed}`]);
+      this.command.set('');
+      // focus ogni tanto
+      setTimeout(() => {
+        this.scrollToBottom();
+        this.cmdInput.nativeElement.focus()
+      }, 0);
+
+    }
+  }
+
+  /**
+   * metodo per scrollare giu, lo chiamo con l'afterview
+   */
+  scrollToBottom() {
+    const el = this.terminalRef?.nativeElement;
+    if (el) el.scrollTop = el.scrollHeight;
+  }
 }
+
