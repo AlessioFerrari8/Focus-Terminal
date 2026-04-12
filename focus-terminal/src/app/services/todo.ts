153012d11b199f81d8ca7e575e69feb2e92386fa
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-04-12 21:07:37 +0200

    Feat: implement Todo service for task management

---
+++ focus-terminal/src/app/services/todo.ts
@@ -0,0 +1,37 @@
+import { Injectable, signal, WritableSignal } from '@angular/core';
+
+
+export type Todos = {
+  id: number;
+  text: string;
+  done: boolean;
+}
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Todo {
+  private tasks: WritableSignal<Todos[]> = signal<Todos[]>([]);
+
+
+  add(text: string): void {
+    // restituisco un nuovo 
+    this.tasks.update(old => [...old, { id: old.length + 1, text, done: false }]);
+  }
+
+  complete(n: number): void {
+    // indice - 1 perché si parte da 0
+    this.tasks.update(old =>
+      old.map((task, i) => i === n - 1 ? { ...task, done: true } : task)
+    );
+  }
+
+  getAll(): string[] {
+    // ritorno array con tutte task
+    return this.tasks().map((task, i) =>
+      `  ${i + 1}. [${task.done ? 'x' : ' '}] ${task.text}`
+    );
+  }
+
+}
