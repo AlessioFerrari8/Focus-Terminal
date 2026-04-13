da679494141f2fd41c40abca0264778f365415de
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-04-13 17:52:51 +0200

    refactor: remove AngularFire dependencies and initialize Firebase in separate config file

--- focus-terminal/src/app/app.config.ts
+++ focus-terminal/src/app/app.config.ts
@@ -1,20 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { firebaseConfig } from './firebase.config';
 import { routes } from './app.routes';
 
-// config firebaes
+// Inizializza Firebase nel firebase.config.ts
+import './firebase.config';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
   ]
 };
