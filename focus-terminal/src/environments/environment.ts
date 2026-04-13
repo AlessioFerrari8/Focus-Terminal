5c9076c2d467e330ae5beb0072276b047995339d
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-04-13 18:44:01 +0200

    feat: add Firebase configuration management and remove deprecated example file

---
+++ focus-terminal/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  configUrl: '/config.json',
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
+  }
+};
