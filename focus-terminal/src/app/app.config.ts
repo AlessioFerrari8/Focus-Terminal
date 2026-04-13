ce78384138322d8cce2e5cb39f5a503b0ffe1277
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-04-13 17:29:26 +0200

    Feat: update app configuration to include HTTP client and Firebase services

--- focus-terminal/src/app/app.config.ts
+++ focus-terminal/src/app/app.config.ts
@@ -1,11 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { firebaseConfig } from './firebase.config';
 import { routes } from './app.routes';
 
+// config firebaes
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ]
 };
